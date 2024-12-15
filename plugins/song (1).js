e3d13c6faef9068f7af4cfff60878c5656809ea5
ytestbot <kavishkatharushan990@gmail.com>
2024-12-15 09:40:55 -0800

    Add files via upload

---
+++ plugins/song (1).js
@@ -0,0 +1,45 @@
+       const {cmd , commands} = require('../command')
+       const {runtime} = require('../lib/functions')
+       const config = require('../config')
+       const yts = require('yt-search')
+function extractYouTubeId(url) {
+    const regex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:watch\?v=|embed\/|v\/|shorts\/|playlist\?list=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
+    const match = url.match(regex);
+    return match ? match[1] : null;
+}
+
+// Function to convert any YouTube URL to a full YouTube watch URL
+function convertYouTubeLink(q) {
+    const videoId = extractYouTubeId(q);
+    if (videoId) {
+        return `https://www.youtube.com/watch?v=${videoId}`;
+    }
+    return q;
+}
+
+const formatViews = views => views >= 1_000_000_000 ? `${(views / 1_000_000_000).toFixed(1)}B` : views >= 1_000_000 ? `${(views / 1_000_000).toFixed(1)}M` : views >= 1_000 ? `${(views / 1_000).toFixed(1)}K` : views.toString();
+        cmd({
+        pattern: "song2", 
+	desc: "Download songs", 
+	category: "download", 
+	filename: __filename }, 
+    async (conn, mek, m, { from, q, reply }) => { 
+	    try { 
+		    if (!q) { 
+	    await conn.sendPresenceUpdate('recording', from); 
+			    await conn.sendMessage(from, { audio: { url: 'https://github.com/themiyadilann/DilaMD-Media/raw/main/voice/song.mp3' }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek }); return; } 
+		    const search = await yts(q); 
+		    const data = search.videos[0]; 
+		    const url = data.url; 
+		    let desc = `> VAJIRA MD YTDL\n\n🎶 *𝗧𝗶𝘁𝗹𝗲*: _${data.title}_\n👤 *𝗖𝗵𝗮𝗻𝗻𝗲𝗹*: _${data.author.name}_\n📝 *𝗗𝗲𝘀𝗰𝗿𝗶𝗽𝘁𝗶𝗼𝗻*: _${data.description}_\n⏳ *𝗧𝗶𝗺𝗲*: _${data.timestamp}_\n⏱️ *𝗔𝗴𝗼*: _${data.ago}_\n👁️‍🗨️ *𝗩𝗶𝗲𝘄𝘀*: _${formatViews(data.views)}_\n🔗 *𝗟𝗶𝗻𝗸*: ${url}`; 
+		    await conn.sendPresenceUpdate('typing', from); 
+		    await conn.sendMessage(from, { image: { url: data.thumbnail }, caption: desc }, { quoted: mek }); 
+		    let down = await fg.yta(url); 
+		    let downloadUrl = down.dl_url; 
+		    await conn.sendPresenceUpdate('recording', from); 
+		    await conn.sendMessage(from, { audio: { url: downloadUrl }, mimetype: "audio/mpeg" }, { quoted: mek }); 
+		  
+	    } catch (e) { 
+		    console.log(e); 
+		    reply(`Error: ${e.message}`); 
+	    } });
